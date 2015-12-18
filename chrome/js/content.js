8da6b5b22d853d705f4d5afa19e5f70bc2ce1c5d
Chris Sciolla <csciolla1@gmail.com>
2015-12-18 13:16:58 -0500

    add the dom walker to the mutation observer

--- chrome/js/content.js
+++ chrome/js/content.js
@@ -7,50 +7,53 @@ function walkTheDOM(node, func) {
   }
 }
 
-function getFromSyncStorage() {
+function getFromSyncStorage(cb) {
   chrome.storage.sync.get(function (items) {
-
-    walkTheDOM(document, function (node) {
-      if (node.nodeType === 3) {
-        replaceText(node);
-      }
-    });
-
-    function replaceText(textNode) {
-      var text = textNode.data;
-
-      if(items && items.itemsStorage) {
-        var newItemsArr = items.itemsStorage.itemsArr;
-        newItemsArr.forEach(function (el,i,arr) {
-          text = text.replace(el.o, el.r);
-        });
-      }
-      textNode.data = text;
-    }
-
-    var observer = new MutationObserver(function(mutations) {
-      try {
-        mutations.forEach(function(mutation) {
-          if (mutation.addedNodes) {
-            [].slice.call(mutation.addedNodes).forEach(function(node) {
-              if (node.nodeName.toLowerCase() == "#text") {
-                replaceText(node);
-              }
-            });
-          }
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    });
-
-    observer.observe(document, {
-      childList: true,
-      subtree:   true
-    });
+    cb(items);
   });
 }
 
-setTimeout(function() {
-  getFromSyncStorage();
-}, 50);
+function doReplace(items) {
+  walkTheDOM(document, function (node) {
+    if (node.nodeType === 3) {
+      replaceText(node);
+    }
+  });
+
+  function replaceText(textNode) {
+    var text = textNode.data;
+
+    if(items && items.itemsStorage) {
+      var newItemsArr = items.itemsStorage.itemsArr;
+      newItemsArr.forEach(function (el,i,arr) {
+        text = text.replace(el.o, el.r);
+      });
+    }
+    textNode.data = text;
+  }
+
+  var observer = new MutationObserver(function(mutations) {
+    try {
+      mutations.forEach(function(mutation) {
+        if (mutation.addedNodes) {
+          [].slice.call(mutation.addedNodes).forEach(function(node) {
+            walkTheDOM(node, function (node) {
+              if (node.nodeType === 3) {
+                replaceText(node);
+              }
+            });
+          });
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree:   true
+  });
+}
+
+getFromSyncStorage(doReplace);
